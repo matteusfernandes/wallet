088e1365dd62823ae7d9d8e9a5a40693818b9618
Matteus Fernandes <matteus_gbi@hotmail.com>
2021-08-30 15:12:39 -0300

    Cria os campos de email e senha e botão de entrar

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-class Login extends React.Component {
+class Login extends Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <div>
+        <label htmlFor="email">
+          E-mail:
+          <input type="email" name="email" data-testid="email-input" />
+        </label>
+
+        <label htmlFor="password">
+          Senha:
+          <input type="password" name="password" data-testid="password-input" />
+        </label>
+        <button type="submit">Entrar</button>
+      </div>
+    );
   }
 }
 
